b9af6641bcc25665e9fbe77f96b141b49d7dc753
Dmitry <dmitrydohy@gmail.com>
2023-01-28 14:24:13 -0800

    Update main.js
    Created working rock, paper, scissors game that prompts user to enter text. Plays rock, paper, scissors with computer as expected. When user enters something other than rock, paper, or scissors, console prompts user to refresh page to try again.

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-let hand = ["Rock","Paper","Scissors"];
+let hand = ["rock","paper","scissors"];
 
 function getComputerChoice(){
     return Math.floor(Math.random() * 3);
 }
 
-console.log(hand[getComputerChoice()]);
+function playRound(playerChoice, computerChoice){
+    if (playerChoice.toLowerCase() == computerChoice){
+        return "Draw!";
+    } else if ((playerChoice.toLowerCase() == "rock" && computerChoice == "paper")
+    || (playerChoice.toLowerCase() == "paper" && computerChoice == "scissors")
+    || (playerChoice.toLowerCase() == "scissors" && computerChoice == "rock")) {
+        return "Computer wins!";
+    } else if (playerChoice.toLowerCase() != "rock" || "paper" || "scissors") {
+        return "You did it wrong, goofy! Refresh the page."
+    } else {
+        return "Player wins!"
+    }
+}
+
+let computerChoice = hand[getComputerChoice()];
+let playerChoice = prompt();
+
+console.log(playRound(playerChoice, computerChoice));
