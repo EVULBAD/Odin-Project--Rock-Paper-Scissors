9444f0db43f56c5dc328b7b66940f22e65affd00
Dmitry <dmitrydohy@gmail.com>
2023-01-28 15:36:12 -0800

    Finished best-out-of-five Javascript game
    Added function that loops the rock, paper, scissors game 5 times and decides who wins by keeping score. Still functions even when user inputs variable that isn't rock, paper, or scissors; it pauses the number of games where it was at before invalid response until user enters a valid one.

--- main.js
+++ main.js
@@ -6,19 +6,47 @@ function getComputerChoice(){
 
 function playRound(playerChoice, computerChoice){
     if (playerChoice.toLowerCase() == computerChoice){
-        return "Draw!";
+        return null
     } else if ((playerChoice.toLowerCase() == "rock" && computerChoice == "paper")
     || (playerChoice.toLowerCase() == "paper" && computerChoice == "scissors")
     || (playerChoice.toLowerCase() == "scissors" && computerChoice == "rock")) {
-        return "Computer wins!";
-    } else if (playerChoice.toLowerCase() != "rock" || "paper" || "scissors") {
-        return "You did it wrong, goofy! Refresh the page."
+        return "comwin"
+    } else if (playerChoice.toLowerCase() != ("rock" || "paper" || "scissors")) {
+        return "bad"
     } else {
-        return "Player wins!"
+        return "playwin"
     }
 }
 
-let computerChoice = hand[getComputerChoice()];
-let playerChoice = prompt();
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++){
+        let computerChoice = hand[getComputerChoice()];
+        let playerChoice = prompt();
+        if (playRound(playerChoice, computerChoice) == "comwin") {
+            computerScore = computerScore + 1;
+        } else if (playRound(playerChoice, computerChoice) == "playwin") {
+            playerScore = playerScore + 1;
+        } else if (playRound(playerChoice, computerChoice) == "bad") {
+            if (i < 0) {
+                // do nothing
+            } else {
+                i = i - 1;
+            }
+        } else {
+            // do nothing
+        }
+    }
+    if (computerScore > playerScore) {
+        return "Computer wins!"
+    } else if (playerScore > computerScore) {
+        return "Player wins!"
+    } else if (playerScore === computerScore) {
+        return "Draw!"
+    } else {
+        // do nothing
+    }
+}
 
-console.log(playRound(playerChoice, computerChoice));
+console.log(game());
